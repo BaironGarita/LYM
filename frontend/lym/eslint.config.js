432e6030167ddf07c32a7fad07b46bb4ca0db03e
Bairon Garita Ballestero <ingbairongarita@gmail.com>
2025-06-23 14:44:18 -0600

    feat: update package.json with new dependencies and scripts
    - Added new dependencies: @emotion/react, @emotion/styled, @fontsource/roboto, @hookform/resolvers, @mui/icons-material, @mui/material, @mui/styled-engine-sc, axios, jwt-decode, nodemon, prop-types, react-hook-form, react-hot-toast, styled-components, and yup.
    - Introduced new scripts for starting the development server and prettier checks.
    
    refactor: remove unused CSS and App component
    
    - Deleted App.css and App.jsx as they contained unused styles and code.
    - Cleaned up index.css for better structure and readability.
    
    chore: configure Vite with jsconfig paths
    
    - Updated vite.config.js to include jsconfig-paths plugin for better path resolution.
    
    chore: add Prettier configuration
    
    - Created .prettierrc.json and .prettierignore for consistent code formatting.
    
    feat: add layout components
    
    - Created Footer, Header, and Layout components for better structure.
    
    feat: implement MUI theme
    
    - Added theme.js to define a custom Material-UI theme for the application.

--- frontend/lym/eslint.config.js
+++ frontend/lym/eslint.config.js
@@ -1,33 +1,27 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
 
 export default [
-  { ignores: ['dist'] },
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { 
+    ecmaVersion: 2020,
+    globals: {...globals.browser, ...globals.node},
+    parserOptions: {
+      ecmaVersion: "latest",
+      ecmaFeatures: { jsx: true },
+      sourceType: "module",
+    },
+  }},
   {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+    settings: { react: { version: "18.3" } },
   },
-]
+ 
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    ignores: ['node_modules', 'dist', 'public', '.nuxt']
+  },
+  eslintConfigPrettier,  
+];

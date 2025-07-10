6b0c74e6464f7d670b5c8df0778386e355b6c624
Bairon Garita Ballestero <ingbairongarita@gmail.com>
2025-07-09 21:36:26 -0600

    feat: update CORS headers to include Authorization and handle preflight requests

--- api/index.php
+++ api/index.php
@@ -3,10 +3,16 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type , Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(204); // No Content
+    exit;
+}
 
 // Composer autoloader
 require_once 'vendor/autoload.php';
